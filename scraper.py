12c4c94df92f3f6dfc147b630359e65a87202a78
hiteshbhatia3559 <31386280+hiteshbhatia3559@users.noreply.github.com>
2019-02-07 02:09:35 +0530

    Initial commit

--- scraper.py
+++ scraper.py
@@ -1,2 +1,15 @@
-import os
-import json
+import re
+import requests
+
+url = "https://medium.com/"
+
+response = requests.request("GET",url).text
+medium_link = re.findall(r'"((https)s?://medium\.com/.*?)"',response)
+
+new_list = [] #has all the urls
+for item in medium_link:
+    (x,y) = item
+    if x != 'https://medium.com/':
+        new_list.append(x)
+
+print(len(new_list))
